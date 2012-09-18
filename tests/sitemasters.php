bdd46fb1aed142026e6bda8a17a446f2197896a9
WouterJ <waldio.webdesign@gmail.com>
2012-09-18 19:01:41 +0200

    Added sitemasters benchmark

---
+++ tests/sitemasters.php
@@ -0,0 +1,35 @@
+<?php
+
+$research = new Research('Lightweight random');
+
+$research->addTest(new Test('Martijn 1', function() {
+    if(time()%2==0){echo 'a';}else{echo'b';}
+}));
+
+$research->addTest(new Test('UpLink', function() {
+    echo substr('ab',rand(0,1),1);
+}));
+
+$research->addTest(new Test('koosax', function() {
+    $a=array('a','b');
+    echo $a[array_rand($a)];
+}));
+
+$research->addTest(new Test('Martijn 2', function() {
+    $a=array('a','b');
+    echo current(shuffle($a));
+}));
+
+$research->addTest(new Test('Pieter', function() {
+    echo (mt_rand(6,7)===6)?'a':'b';
+}));
+
+$research->addTest(new Test('Wouter', function() {
+    (mt_rand(0,1)&&print'a')||print'b';
+}));
+
+$research->addTest(new Test('Martijn 3', function() {
+    echo uniqid()%2===0?'a':'b';
+}));
+
+$research->runTests();
